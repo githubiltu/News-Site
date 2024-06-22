f2886084f75a005ccaa9c14cc183d38f793adbd2
gitbubiltu <biltupal20190@gmail.com>
2024-06-22 22:46:44 +0530

    commit message

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2019 News | Powered by <a href=" https://githubiltu.github.io/Presonal-Website/">Biltu Pal</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
